39e119d8fe9e16e14a2f02bdf85ece16e260148c
Tamal Saha <tamal@appscode.com>
2021-11-06 19:00:34 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ generate-connect-command/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name string
+	Email string
+}
+
+func main() {
+	u := User{
+		Name:  "Tamal Saha",
+		Email: "tamal@appscode.com",
+	}
+	fmt.Println(u)
+
+	// make to verified
+	// get token
+	// helm command
+	// render and get kubectl link
+}
