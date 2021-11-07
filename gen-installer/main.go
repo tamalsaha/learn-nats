291b54e2a37a083c99a81eeb7dea3d9ce6aac28c
Tamal Saha <tamal@appscode.com>
2021-11-07 12:35:43 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- gen-installer/main.go
+++ gen-installer/main.go
@@ -5,18 +5,25 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"kubepack.dev/kubepack/apis/kubepack/v1alpha1"
-	"kubepack.dev/kubepack/pkg/lib"
-
 	"github.com/google/uuid"
 	flag "github.com/spf13/pflag"
+	"gomodules.xyz/blobfs/testing"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog/v2"
+	"kubepack.dev/kubepack/apis"
+	"kubepack.dev/kubepack/apis/kubepack/v1alpha1"
+	"kubepack.dev/kubepack/pkg/lib"
 	"sigs.k8s.io/yaml"
 )
 
 var (
 	file = "artifacts/kubedb-community/order.yaml"
+)
+
+const (
+	YAMLBucket                   = "gs://connect.bytebuilders.link"
+	YAMLHost                     = "https://connect.bytebuilders.link"
+	GoogleApplicationCredentials = "/Users/tamal/AppsCode/credentials/license-issuer@appscode-domains.json"
 )
 
 func main() {
@@ -79,4 +86,16 @@ func main_helm3() {
 		klog.Fatal(err)
 	}
 	fmt.Println(string(data))
+}
+
+func NewTestBlobStore() (*lib.BlobStore, error) {
+	fs, err := testing.NewTestGCS(apis.YAMLBucket, GoogleApplicationCredentials)
+	if err != nil {
+		return nil, err
+	}
+	return &lib.BlobStore{
+		BlobFS: fs,
+		Host:   apis.YAMLHost,
+		Bucket: apis.YAMLBucket,
+	}, nil
 }
