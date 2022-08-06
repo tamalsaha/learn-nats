77ef0e607bd72d6879dfe964bda8bd989a646ad9
Tamal Saha <tamal@appscode.com>
2022-08-06 04:33:56 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- logr-demo/main.go
+++ logr-demo/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/nats-io/nats.go"
+	"log"
 	"os"
 	"sync"
 	"time"
@@ -75,9 +76,24 @@ func NewStdoutLogger() logr.Logger {
 	}, funcr.Options{})
 }
 
+/*
+nats-server -p 4222 -m 8222 -js --user myname --pass password
+
+nats sub msg.test --user myname --password password --connection-name=demo
+*/
 func main() {
-	l := NewStdoutLogger()
+	// Set a user and plain text password
+	nc, err := nats.Connect("127.0.0.1", nats.UserInfo("myname", "password"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer nc.Close()
+	defer nc.Drain()
+
+	l := NewAsync(nc, "msg.test")
 	l.Info("default info log", "stringVal", "value", "intVal", 12345)
 	l.V(0).Info("V(0) info log", "stringVal", "value", "intVal", 12345)
 	l.Error(fmt.Errorf("an error"), "error log", "stringVal", "value", "intVal", 12345)
+
+	select {}
 }
