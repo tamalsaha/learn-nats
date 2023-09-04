82809533e7e0a6cfbfc5e12d7f92e241b58192db
Tamal Saha <tamal@appscode.com>
2023-09-04 03:48:03 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ reconnect-demo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tamalsaha/learn-nats/natsclient"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	nc, err := natsclient.NewConnection(nats.DefaultURL, "")
+	if err != nil {
+		panic(err)
+	}
+	defer nc.Drain()
+
+	nc.Publish("greet.joe", []byte("hello"))
+
+	sub, _ := nc.SubscribeSync("greet.*")
+
+	msg, _ := sub.NextMsg(10 * time.Millisecond)
+	fmt.Println("subscribed after a publish...")
+	fmt.Printf("msg is nil? %v\n", msg == nil)
+
+	nc.Publish("greet.joe", []byte("hello"))
+	nc.Publish("greet.pam", []byte("hello"))
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+	nc.Publish("greet.bob", []byte("hello"))
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+}
